40525b2ed15ab1bf989456bc7d2101113ca7561e
Tais Torres <taisincollege@gmail.com>
2025-02-16 21:06:12 -0500

    Task 7 - Budget Tracker

--- cc_7.js
+++ cc_7.js
@@ -20,7 +20,7 @@ console.log(`Hourly Wage: $${calculateHourlyWage(75000, 35)}`); // Expected outp
 const calculateLoyaltyDiscount = (amount, years) => {
     let loyaltyDiscount = 0
     if (years >= 5) loyaltyDiscount = amount *.85;
-     else if (years >= 3) loyaltyDiscount = amount *.90;
+     else if (years >= 3) loyaltyDiscount = amount *.90;       //loyaty discounts based on the total years of membership
      else if (years < 3) loyaltyDiscount = amount * .95;
         
      console.log(`Discounted Price: $${loyaltyDiscount.toFixed(2)}`);
@@ -34,7 +34,7 @@ function calculateShippingCost(weight, location, expedited = false) {
     if (location === "USA") totalCost = (weight * .5) + 5
     else if (location ==="Canada") totalCost = (weight * .7) + 10
 
-    if (expedited) totalCost += 10
+    if (expedited) totalCost += 10     //Used only if expedited shipping is true
 
     console.log(`Shipping Cost: $${totalCost.toFixed(2)}`);
 }
@@ -43,7 +43,7 @@ calculateShippingCost(5, "Canada", false); // Expected output: "Shipping Cost: $
 
 //Task 5
 function calculateLoanInterest(principal, rate, years) {
-    let interest = (principal * rate) * years
+    let interest = (principal * rate) * years                     //equation for the loan interest over x years
     console.log(`Total Loan Interest: $${interest.toFixed(2)}`);
 }
 calculateLoanInterest(1000, 0.05, 3); // Expected output: "Total Interest: $150.00"
@@ -53,9 +53,23 @@ calculateLoanInterest(5000, 0.07, 5); // Expected output: "Total Interest: $1750
 let transactions = [500, 1200, 3000, 800, 2200];
 
 function filterHighValueTransactions( transactions, amount) {
-    let highValue = transactions.filter(amount);
+    let highValue = transactions.filter(amount);                //filter out all the maounts under 1000
     console.log(`High Value Orders: ${highValue}`);
 }
 
 filterHighValueTransactions(transactions, amount => amount > 1000);
 // Expected output: [1200, 3000, 2200]
+
+//Task 7
+function createBudgetTracker() {
+    let totalBudget = 0;
+
+    return function (expense) {
+    totalBudget -= expense;            // subtracts the expenses from the budget
+    return `Current Balance: $${totalBudget}`;
+    }
+}
+
+let budget = createBudgetTracker();
+console.log(budget(300)); // Expected output: "Current Balance: -$300"
+console.log(budget(200)); // Expected output: "Current Balance: -$500"
