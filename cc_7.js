c5a45e4b102ed560da4ec342eba739860f7b0f58
Tais Torres <taisincollege@gmail.com>
2025-02-16 20:48:33 -0500

    Task 5 - Business Loan Interest Calculation

--- cc_7.js
+++ cc_7.js
@@ -40,3 +40,11 @@ function calculateShippingCost(weight, location, expedited = false) {
 }
 calculateShippingCost(10, "USA", true); // Expected output: "Shipping Cost: $20.00"
 calculateShippingCost(5, "Canada", false); // Expected output: "Shipping Cost: $13.50"
+
+//Task 5
+function calculateLoanInterest(principal, rate, years) {
+    let interest = (principal * rate) * years
+    console.log(`Total Loan Interest: $${interest.toFixed(2)}`);
+}
+calculateLoanInterest(1000, 0.05, 3); // Expected output: "Total Interest: $150.00"
+calculateLoanInterest(5000, 0.07, 5); // Expected output: "Total Interest: $1750.00"
